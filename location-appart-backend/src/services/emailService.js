1288c3aaf0e2b006dd8a2e96e560ff70dbbbcea4
juleswjr <wejrochjules@gmail.com>
2026-02-14 06:53:13 +0100

    fix email sending4

--- location-appart-backend/src/services/emailService.js
+++ location-appart-backend/src/services/emailService.js
@@ -12,8 +12,8 @@ const transporter = nodemailer.createTransport({
 });
 */
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-  port: parseInt(process.env.EMAIL_PORT || '465'), // Force 465
+  host: 'smtp.gmail.com',
+  port:  '465', // Force 465
   secure: true, // Vrai pour le port 465
   auth: {
     user: process.env.EMAIL_USER,
