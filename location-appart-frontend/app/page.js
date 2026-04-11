b62fc9a7ea2ae1b001f1657229dcc8a882f4f80e
juleswjr <wejrochjules@gmail.com>
2026-04-11 15:01:55 +0200

    update page

--- location-appart-frontend/app/page.js
+++ location-appart-frontend/app/page.js
@@ -93,7 +93,7 @@ export default async function Home() {
           Nos Appartements
         </h2>
         
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12">
           {apartments.map((appart) => (
             <ApartmentCard key={appart.id} apartment={appart} />
           ))}
